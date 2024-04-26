1e7d40fc2987eda1847d969d7025d1244f7a5bd6
Daniel Hirsch <daniel@hirschtechsolutions.com>
2024-04-26 06:59:31 -0700

    Update migration

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  project_url TEXT := 'https://mcvusrlcyztkgwrgyvnc.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jdnVzcmxjeXp0a2d3cmd5dm5jIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNDEzOTU1OCwiZXhwIjoyMDI5NzE1NTU4fQ.qzl1QSPbZk1coCS-SODPEtjtTow7b6hi84UPuxaejJg'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
